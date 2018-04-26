dcd50ced1e40aaf908f500c0ace0cc8e20ffd4d2
seirios <seirios@buaa.edu.cn>
2018-04-27 00:20:36 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/web_Tiny.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/web_Tiny.dir/main.cpp.o"
+  "CMakeFiles/web_Tiny.dir/csapp.cpp.o"
+  "web_Tiny.pdb"
+  "web_Tiny"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/web_Tiny.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
